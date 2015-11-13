2f1fa97388c0e123681b7833531d8f7adb4684d3
Ivan <itrubchaninov@list.ru>
2015-11-13 18:57:28 +0300

    первый шаг
    Прописал базовую модель класса персонажа

---
+++ Person_02.py
@@ -0,0 +1,25 @@
+import random
+
+import Inventar
+
+class Person:
+    h=1
+    hels=10
+    damage=2
+    def gan(self):
+        pass
+    def inv(self):
+        pass
+    def __init__(self,name):
+        self.name=name
+    def shoot(self):
+        pass
+    def dam(self,ur):
+        self.hels-=ur
+        print("Нанесено урона "+str(ur)+" Здоровья осталось "+ str(self.hels))
+    def dead(self):
+        print('Персонаж '+ str(self.name)+' погиб.')
+
+
+
+
