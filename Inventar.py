a83cbb1b463e871c1c24c7c9a03119e6a4cfc219
Ivan <itrubchaninov@list.ru>
2015-11-14 21:25:36 +0300

    оружие
    Вношу механику выбора оружия

---
+++ Inventar.py
@@ -0,0 +1,16 @@
+import random
+
+
+class items():    
+    def igun(self,sel):
+        if sel =='пистолет':
+            d=[1,2,3,4]
+            return d
+        if sel =='автомат':
+            d=[3,4]
+            return d
+        if sel =='пулемёт':
+            d=[1,2,5]
+            return d
+
+    
